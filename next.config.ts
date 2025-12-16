0bd39aa8cba569d1c8d08b8e96cd58fad5f2b5f5
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-12-16 13:27:22 +0530

    Add new files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["images.unsplash.com","cms.amraj.in"],
-  },
-  /* config options here */
   reactStrictMode: true,
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
